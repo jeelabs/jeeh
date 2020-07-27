1625d3e88f6e66e9ae7025cc01c375a324b604ef
Jean-Claude Wippler <jc@wippler.nl>
2020-07-27 03:02:07 +0200

    400 MHz PLL now ok, but only with non-irq uart (?)

--- arch/stm32h7.cpp
+++ arch/stm32h7.cpp
@@ -3,17 +3,24 @@
 #include <jee.h>
 
 void enableClkAt400MHz () {
+    Periph::bitClear(Periph::pwr+0x0C, 2); // CR3 SCUEN
     MMIO32(Periph::pwr+0x18) = (0b11<<14); // D3CR VOS = Scale 1
     while (Periph::bit(Periph::pwr+0x18, 13) == 0) {} // wait for VOSRDY
     MMIO32(Periph::flash+0x00) = 0x24; // flash acr, 4 wait states
 
+    Periph::bitSet(Periph::rcc+0x154, 1); // APB4ENR SYSCFGEN
+    constexpr uint32_t syscfg = 0x58000400;
+    Periph::bitSet(syscfg+0x20, 0); // CCCSR EN (I/O compensation)
+
+    MMIO32(Periph::rcc+0x28) = (4<<4) | (0b10<<0); // prescaler w/ HSE
     Periph::bitSet(Periph::rcc+0x00, 16); // HSEON
     while (Periph::bit(Periph::rcc+0x00, 17) == 0) {} // wait for HSERDY
 
 //  MMIO32(Periph::rcc+0x28) = (XTAL<<4) | (0b10<<0); // prescaler w/ HSE
-//  MMIO32(Periph::rcc+0x30) = (1<<24) | (2<<16) | (1<<9) | (400<<0);
+//  MMIO32(Periph::rcc+0x30) = (0<<24) | (1<<16) | (0<<9) | (399<<0);
     MMIO32(Periph::rcc+0x28) = (4<<4) | (0b10<<0); // prescaler w/ HSE
-    MMIO32(Periph::rcc+0x30) = (1<<24) | (3<<16) | (1<<9) | (99<<0);
+    MMIO32(Periph::rcc+0x2C) = 0x01FF0000; // why? isn't this the pwrup default?
+    MMIO32(Periph::rcc+0x30) = (1<<24) | (3<<16) | (1<<9) | (399<<0);
     Periph::bitSet(Periph::rcc+0x00, 24); // PLL1ON
     while (Periph::bit(Periph::rcc+0x00, 25) == 0) {} // wait for PLL1RDY
     MMIO32(Periph::rcc+0x18) = (0b100<<4) | (0b1000<<0); // APB3/2, AHB/2
